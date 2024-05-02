a4e277db2faa56ae482c9e9c7c0e227a1388414a
Robert Godlewski <robtechg5@gmail.com>
2024-05-01 17:36:29 -0700

    adding in gravity and movement function for characters

--- game/__init__.py
+++ game/__init__.py
@@ -6,7 +6,6 @@ from .character import Character
 class Platformer:
     def __init__(self) -> None:
         pygame.init()
-        # Tupple in RGB
         self.bg_color = (255,255,255)
         self.width = 1280
         self.height = 720
@@ -15,8 +14,11 @@ class Platformer:
         self.screen = pygame.display.set_mode((self.width, self.height))
         pygame.display.set_caption("Platformer")
         player_size = 20
-        self.player = Character((255,0,0), player_size, player_size, self.width/2, self.height-player_size)
-
+        # Fix the positioning of the character
+        # Bottom of the screen
+        # self.player = Character((255,0,0), player_size, player_size, self.width/2, self.height-player_size)
+        # Top of the screen - testing gravity
+        self.player = Character((255,0,0), player_size, player_size, self.width/2, player_size)
 
     def run(self):
         clock = pygame.time.Clock()
@@ -34,15 +36,7 @@ class Platformer:
             self.screen.fill(self.bg_color)
 
             self.player.draw(self.screen)
-            keys = pygame.key.get_pressed()
-            # if keys[pygame.K_UP] or keys[pygame.K_w]:
-            #     player_pos.y -= 300 * dt
-            # if keys[pygame.K_DOWN] or keys[pygame.K_s]:
-            #     player_pos.y += 300 * dt
-            if keys[pygame.K_LEFT]:
-                self.player.move_left(dt)
-            if keys[pygame.K_RIGHT]:
-                self.player.move_right(dt)
+            self._handle_move(dt, self.player)
 
             # flip() the display to put your work on screen
             pygame.display.flip()
@@ -51,3 +45,16 @@ class Platformer:
             dt = clock.tick(self.fps)/1000
 
         pygame.quit()
+
+    def _handle_move(self, dt, character: Character):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            character.move_left(dt)
+        if keys[pygame.K_RIGHT]:
+            character.move_right(dt)
+        # Gravity test - Fix later
+        if keys[pygame.K_SPACE]:
+            self.player.gravity_affect(self.fps)
+        else:
+            self.player.y_velocity = 0
+            self.player.fall_count = 0
--- game/character.py
+++ game/character.py
@@ -3,11 +3,14 @@ import pygame
 
 class Character(pygame.sprite.Sprite):
     def __init__(self, color: tuple, width: int, height: int, position_x: int, position_y: int):
+        super().__init__()
         # For now just use a rectangle
         self.color = color
         self.rect = pygame.Rect(position_x,position_y,width,height)
-        self.x_velocity = 5
-        self.y_velocity = 5
+        self.x_velocity = 0
+        self.y_velocity = 0
+        self.gravity = -9.8
+        self.fall_count = 0
 
     def draw(self, surface) -> pygame.Rect:
         pygame.draw.rect(surface, color=self.color, rect=self.rect)
@@ -21,3 +24,8 @@ class Character(pygame.sprite.Sprite):
 
     def move_right(self, dt):
         self.rect.x += 300 * dt
+
+    def gravity_affect(self, fps):
+        self.y_velocity += (self.fall_count/fps)*self.gravity
+        self.move(self.x_velocity, -self.y_velocity)
+        self.fall_count += 1
